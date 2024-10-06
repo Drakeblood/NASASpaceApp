2d47e94f8cff6e1ae7c89a241baf035c1e8061b6
Drakeblood <56769943+Drakeblood@users.noreply.github.com>
2024-10-06 14:34:25 +0200

    build

---
+++ Source/NASASpaceApp/Public/GameModes/NASAGameMode.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NASAGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NASASPACEAPP_API ANASAGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
